158cc1e42a0fa346fc1d95ebd9f396111334f9f8
Tyler Hoff <tylehoff@umich.edu>
2017-09-22 09:14:56 -0400

    first commit

---
+++ flickr_response.py
@@ -0,0 +1,25 @@
+import json
+import unittest
+import random
+
+
+class Photo(object):
+    def __init__(self, fdict):
+        self.title = fdict['photo']['title']['_content']
+        self.artist = fdict['photo']['owner']['username']
+        tg = []
+        for e in fdict['photo']['tags']['tag']:
+            tg.append(e['_content'])
+        self.tags = tg
+        self.dateuploaded = fdict['photo']['dateuploaded']
+
+    def __str__(self):
+        return "{} by {}".format(self.title,self.artist)
+
+with open('sample_diction.json', 'r') as f:
+    sdict = f.read()
+picdict = json.loads(sdict)
+
+pic = Photo(picdict)
+
+print (pic)
